6d080b34cb2e9a077bab14474502a4dccd65910e
D074034 <robin.schleser@sap.com>
2020-09-27 20:57:26 +0200

    Unity HSS Workshop builder

---
+++ Assets/SchleserORG/Scripts/MakeContactTP.cs
@@ -0,0 +1,107 @@
+﻿using OculusSampleFramework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MakeContactTP : MonoBehaviour
+{
+    [Header("Target Positions")]
+    public Vector3[] xyzTarget;
+    public Vector3[] rotTarget;
+    public string[] nameOfObj;
+
+    [Space(20)]
+    [Header("Status indicator Materials")]
+    public Material matCorrect;
+    public Material matStandard;
+    public Material matWrong;
+
+
+    [Space(20)]
+    [Header("Locations of Indicator")]
+    public Vector3[] xyzIndicator;
+    public Vector3[] scaleIndicator;
+
+
+    [Space(20)]
+    [Header("Steps")]
+    public TextMeshPro tmp;
+    public string[] steps;
+
+
+    [Space(20)]
+    [Header("Debug")]
+    public bool debug;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals(nameOfObj[index]))
+        {
+
+
+            // Make UnMovable
+            Destroy(collision.gameObject.GetComponent<BoxCollider>());
+            Destroy(collision.gameObject.GetComponent<Rigidbody>());
+            Destroy(collision.gameObject.GetComponent<DistanceGrabbable>());
+
+            gameObject.GetComponent<MeshRenderer>().material = matCorrect;
+            collision.gameObject.transform.eulerAngles = rotTarget[index];
+            collision.gameObject.transform.position = xyzTarget[index];
+
+            if (index == xyzIndicator.Length)
+            {
+                Destroy(gameObject);
+                tmp.alignment = TextAlignmentOptions.Center;
+                tmp.text = "Geschafft!\nHerzlichen Glückwunsch";
+                return;
+            }
+
+
+            gameObject.transform.position = xyzIndicator[index];
+            gameObject.transform.localScale = scaleIndicator[index];
+            gameObject.GetComponent<MeshRenderer>().material = matStandard;
+
+            
+            if (debug)
+            {
+                string txt = "\n    XYZ: " + collision.gameObject.transform.position.x + " / " + collision.gameObject.transform.position.y + " / " + collision.gameObject.transform.position.z + "\n    ROT: " + collision.gameObject.transform.rotation.x + " / " + collision.gameObject.transform.rotation.y + " / " + collision.gameObject.transform.rotation.z;
+                txt += "\n    XYZ: " + xyzTarget[index].x + " / " + xyzTarget[index].y + " / " + xyzTarget[index].z + "\n    ROT: " + rotTarget[index].x + " / " + rotTarget[index].y + " / " + rotTarget[index].z;
+                tmp.text = txt;
+            }
+            else
+            {
+                tmp.text = "\n     Schritt " + (index + 2) + ":\n     " + steps[index].Replace('2', '\n');
+            }
+
+            index++;
+            
+        }
+        else 
+        {
+            gameObject.GetComponent<MeshRenderer>().material = matWrong;
+        }
+    }
+
+
+    void OnCollisionExit(Collision collision)
+    {
+        gameObject.GetComponent<MeshRenderer>().material = matStandard;
+    }
+
+}
---
+++ Assets/SchleserORG/Scripts/Motorbuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motorbuilder : MonoBehaviour
+{
+
+    public GameObject engineCase;
+    public GameObject engineTop;
+    public GameObject engineEnd;
+    public GameObject engineFlange;
+    public GameObject engineShaft;
+    public GameObject engineGear;
+
+    public Vector3 targetLocCase;
+    public Vector3 targetLocTop;
+    public Vector3 targetLocEnd;
+    public Vector3 targetLocFlange;
+    public Vector3 targetLocShaft;
+    public Vector3 targetLocGear;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
